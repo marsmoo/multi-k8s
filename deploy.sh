2f7fae50fa76a0b6002973ebfbbd63c62dc0e674
Mark Mooney <marsmoo@yahoo.com>
2021-04-05 22:03:57 -0400

    corrected misspellings

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 docker build -t docmarsmoo/multi-client:latest -t docmarsmoo/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t docmarsmoo/multi-server:latest -t docmarsmoo/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t docmarsmoo/multi-worker:latest -t docmarsmoo/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push docsmarsmoo/multi-client:latest
-docker push docsmarsmoo/multi-server:latest
-docker push docsmarsmoo/multi-worker:latest
+docker push docmarsmoo/multi-client:latest
+docker push docmarsmoo/multi-server:latest
+docker push docmarsmoo/multi-worker:latest
 
-docker push docsmarsmoo/multi-client:$SHA
-docker push docsmarsmoo/multi-server:$SHA
-docker push docsmarsmoo/multi-worker:$SHA
+docker push docmarsmoo/multi-client:$SHA
+docker push docmarsmoo/multi-server:$SHA
+docker push docmarsmoo/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=docmarsmoo/multi-server:$SHA
